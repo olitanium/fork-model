0cea23b1dad7184bedd36cc0491915a6300c9c36
Olitanium <oli.harris@outlook.com>
2023-11-29 11:54:44 +0000

    No interior mutability, no references, no lifetimes 1.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -42,33 +42,44 @@ pub trait HasHint {
 pub type CodeLine<C> = fn(&mut C, &<C as HasHint>::Hint) -> Vec<<C as HasHint>::Hint>;
 
 #[derive(Clone)]
-pub struct Process<'a, C: HasHint> {
+pub struct Process<C: HasHint> {
     code: std::rc::Rc<Vec<CodeLine<C>>>,
     instruction_ptr: usize,
-    
-    manager: &'a Manager<'a, C>,
     
     content: C,
     curr_hint: C::Hint,
 }
 
-impl<'a, C: HasHint + core::fmt::Debug> core::fmt::Debug for Process<'a, C> {
+impl<C: HasHint + core::fmt::Debug> core::fmt::Debug for Process<C> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         self.content.fmt(f)
     }
 }
 
-impl<'a, C: HasHint + core::fmt::Display> core::fmt::Display for Process<'a, C> {
+impl<C: HasHint + core::fmt::Display> core::fmt::Display for Process<C> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         self.content.fmt(f)
     }
 }
 
-impl<'a, C: HasHint> Process<'a, C> {
-    pub fn new(content: C, code: Vec<CodeLine<C>>, manager: &'a Manager<'a, C>) -> Self {
+impl<C: HasHint> core::ops::Deref for Process<C> {
+    type Target = C;
+
+    fn deref(&self) -> &Self::Target {
+        &self.content
+    }
+}
+
+impl<C: HasHint> core::ops::DerefMut for Process<C> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.content
+    }
+}
+
+impl<C: HasHint> Process<C> {
+    pub fn new(content: C, code: Vec<CodeLine<C>>) -> Self {
         Self {
             code: std::rc::Rc::new(code),
-            manager,
             content,
             
             instruction_ptr: 0,
@@ -77,16 +88,17 @@ impl<'a, C: HasHint> Process<'a, C> {
     }
 }
 
-impl<'a, C: Clone + HasHint> Process<'a, C> {
-    pub fn execute_fork(&mut self) -> () {
-        for codeline in self.code.clone()[self.instruction_ptr..].iter() {
+impl<C: Clone + HasHint> Process<C> {
+    fn execute_fork(&mut self, manager: &mut Manager<C>) -> () {
+        for codeline in &self.code.clone()[self.instruction_ptr..] {           
+            
             let mut hints = codeline(&mut self.content, &self.curr_hint); // Mutates the process and returns the list of hints for branching
             self.instruction_ptr += 1; // Increment the instruction ptr so if the process needs resuming, it knows where to go
             
             if hints.len() > 0 { // If any hints were created, they need to be destributed
                 if hints.len() > 1 { // If more than one hint was generated, then forks need to occur
                     for hint in hints.drain(1..) { // Drain the hints pool up from index 1, leaving 0 for the original
-                        self.manager.fork(self, hint); // clone the process and assign the hint (nothing else. The manager is not responsible for the execution of code)
+                        manager.fork(self, hint); // clone the process and assign the hint (nothing else. The manager is not responsible for the execution of code)
                     }
                 }
                 self.curr_hint = hints.pop().unwrap(); // There will be only one entry in the vector now, time to pop this
@@ -97,110 +109,121 @@ impl<'a, C: Clone + HasHint> Process<'a, C> {
     }
 }
 
-impl<'a, C: HasHint> Process<'a, C> {
+impl<C: HasHint> Process<C> {
     pub fn execute(&mut self) -> () {
         for codeline in self.code.clone().iter() {
             let mut hints = codeline(&mut self.content, &self.curr_hint); // Mutates the process and returns the list of hints for branching
-            if hints.len() > 0 {
-                self.curr_hint = hints.pop().unwrap(); // There will be only one entry in the vector now, time to pop this
+            match hints.pop() {
+                Some(val) => self.curr_hint = val,
+                None => {}
             }
         }
     }
-    
-    pub fn set_hint(&mut self, hint: C::Hint) {
+}
+
+impl<C: HasHint> Process<C> {
+    fn set_hint(&mut self, hint: C::Hint) {
         self.curr_hint = hint
     }    
 }
 
-pub struct Manager<'a, C: HasHint> {
-    vec_process: core::cell::RefCell<Vec<Box< Process<'a, C> >>>,
-    vec_new_process: core::cell::RefCell<Vec<Box< Process<'a, C> >>>,
-    clock: core::cell::Cell<usize>,  
+pub struct Manager<C: HasHint> {
+    vec: Vec<Process<C>>,
+    clock: usize,
 }
 
-impl<'a, C: HasHint + std::fmt::Debug> std::fmt::Display for Manager<'a, C> {
+
+impl<C: HasHint + std::fmt::Debug> std::fmt::Display for Manager<C> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Clock: {}, Processes: {:?}", self.clock(), self.vec_process.borrow())
+        write!(f, "Clock: {}, Processes: {:?}", self.clock(), self.vec)
     }
 } 
 
-impl<'a, C: HasHint> Manager<'a, C> {
-    pub fn new() -> Self {
+impl<C: HasHint> Manager<C> {
+    pub fn new(content: C, code: Vec<CodeLine<C>>) -> Self {
         Self {
-            vec_process: core::cell::RefCell::new(vec![]),
-            vec_new_process: core::cell::RefCell::new(vec![]),
-            clock: core::cell::Cell::new(0),
+            vec: vec![Process::new(content, code)],
+            clock: 0,
         }
-    }
-
-    pub fn add_process(&self, new_process: Process<'a, C>) {
-        let result_borrowed = self.vec_process.try_borrow_mut();
-        let mut borrowed_vec = result_borrowed.unwrap_or_else(|_| self.vec_new_process.borrow_mut());
-
-        borrowed_vec.push(Box::new(new_process))
-    }
-
-    pub fn clock(&self) -> usize {
-        self.clock.get()
     }
 }
 
-impl<'a, C: HasHint + Clone> Manager<'a, C> {    
-    fn fork(&'a self, process: &mut Process<'a, C>, hint: C::Hint) {
+impl<C: HasHint> Manager<C> {
+    pub fn empty() -> Self {
+        Self {
+            vec: vec![],
+            clock: 0,
+        }
+    }
+}
+
+impl<C: HasHint> Manager<C> {
+    pub fn add_process(&mut self, new_process: Process<C>) {
+        self.vec.push(new_process);
+    }
+}
+
+impl<C: HasHint> Manager<C> {
+    pub fn clock(&self) -> usize {
+        self.clock
+    }
+}
+
+impl<C: HasHint + Clone> Manager<C> {
+    fn fork(&mut self, process: &mut Process<C>, hint: C::Hint) {
         let mut new_process = process.clone();
         new_process.set_hint(hint);
         
         self.add_process(new_process);
     }
+}
 
-    pub fn run_fork(&self) {
+impl<C: HasHint + Clone> Manager<C> {
+    pub fn execute(&mut self) {
         let mut start_index = 0;
-        let mut borrowed_vec = self.vec_process.borrow_mut();
+        let mut temp_manager = Manager::empty();
         
         loop {
-            for process in borrowed_vec[start_index..].iter_mut() {
-                process.execute_fork();
+            for process in &mut self.vec[start_index..] {
+                process.execute_fork(&mut temp_manager);
             }
-            
-            let mut borrowed_vec_new = self.vec_new_process.borrow_mut();
-            if borrowed_vec_new.len() == 0 {
+
+            if temp_manager.vec.len() == 0 {
                 break;
             }
             
-            start_index = borrowed_vec.len();
+            start_index = self.vec.len();
 
-            borrowed_vec.extend(borrowed_vec_new.drain(..));
+            self.vec.extend(temp_manager.vec.drain(..));
         }
         
-        self.clock.set(self.clock() + 1)
+        self.clock += 1
     }
 }
 
-impl<'a, C: HasHint> Manager<'a, C> {    
+/* impl<C: HasHint> Manager<C> {    
     pub fn run(&mut self) {
-        for process in self.vec_process.borrow_mut().iter_mut() {
+        for process in &mut self.vec {
             process.execute();
         }
         
-        self.clock.set(self.clock() + 1)
+        self.clock += 1
     }
-}
+} */
 
-impl<'a, C: HasHint + Ord> Manager<'a, C> {   
-    pub fn prune(&self, range: usize, step: usize) {
-        let mut borrowed_vec = self.vec_process.borrow_mut();
-        if borrowed_vec.len() <= range {
-            return
+impl<C: HasHint + Ord> Manager<C> {   
+    pub fn prune(&mut self, range: usize, step: usize) {
+        if self.vec.len() > range {
+            self.vec.sort_unstable_by(
+                |a, b| std::cmp::Ord::cmp(&b.content, &a.content) // backwards because want greatest at front
+            );
+
+            let mut temp = 
+                self.vec
+                .drain(range + step..)
+                .step_by(step)
+                .collect();
+            self.vec.append(&mut temp);
         }
-
-        
-        borrowed_vec.sort_unstable_by(
-            |a, b| std::cmp::Ord::cmp(&b.content, &a.content) // backwards because want greatest at front
-        );
-        let iter = borrowed_vec.drain(range + step..).step_by(step);
-        
-        let mut borrowed_vec_new = self.vec_new_process.borrow_mut();
-        borrowed_vec_new.extend(iter);
-        borrowed_vec.extend(borrowed_vec_new.drain(..));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,50 @@
-use fork_model;
+use fork_model::{self, HasHint, Process};
 
-#[derive(Clone, Default, PartialEq, Eq, PartialOrd, Ord)]
-struct Wi32(i32);
+type Cargo = f32;
 
-impl fork_model::HasHint for Wi32 {
-    type Hint = Wi32;
+enum VesselMethod {
+    ROAD,
+    SEA,
+    AIR,
 }
 
-impl std::fmt::Debug for Wi32 {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
+enum QueueMode {
+    LOAD,
+    UNLOAD,
+    PASS,
+}
+
+struct VesselConfig {
+    name: std::string::String,
+    method: VesselMethod,
+    capacity: Cargo,
+    speed: f32,
+    volume: i32,
+}
+
+struct Vessel {
+    config: VesselConfig,
+    destination: std::string::String,
+    time_to_run: f32,
+    contents: Cargo,
+    queue_mode: QueueMode, 
+    ready_to_leave: bool,
+}
+
+
+#[derive(Clone, Eq, Ord, PartialEq, PartialOrd, Debug)]
+struct Model {}
+
+impl fork_model::HasHint for Model {
+    type Hint = std::string::String;
 }
 
 fn main() {
-    
-    let code: Vec<fork_model::CodeLine<Wi32>> = vec![
-        |content: &mut Wi32, _: &Wi32| -> Vec<Wi32> {
-            content.0 += 1;
-            return vec![Wi32(1),Wi32(2)];
-        },
-        |content: &mut Wi32, hint: &Wi32| -> Vec<Wi32> {
-            content.0 += hint.0;
-            return vec![];
-        },
-    ];
-        
-    let manager = fork_model::Manager::new();
-    let process = fork_model::Process::new(Wi32(0), code, &manager);
-    manager.add_process(process);
+    let code: Vec<fork_model::CodeLine<Model>> = vec![];
+    let mut manager = fork_model::Manager::new(Model {}, code.clone());
 
     loop {
-        manager.run_fork();
+        manager.execute();
         manager.prune(10, 2);
         println!("{}", manager);
     }
