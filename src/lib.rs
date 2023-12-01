878110f709107ee120d2fe319088494216284a8e
Oliver <oli.harris@outlook.com>
2023-12-01 16:44:39 +0000

    CodeLine type changed to Option<H> to force API users to consider cases where hint is invalid

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-pub type CodeLine<C, H> = fn(&mut C, &H) -> Vec<H>;
+pub type CodeLine<C, H> = fn(&mut C, Option<H>) -> Vec<H>;
 
 #[derive(Clone)]
 pub struct Process<C, H> {
     code: std::rc::Rc<Vec<CodeLine<C, H>>>,
     instruction_ptr: usize,
-    
+
     content: C,
-    curr_hint: H,
+    curr_hint: Option<H>,
 }
 
 impl<C: core::fmt::Debug, H> core::fmt::Debug for Process<C, H> {
@@ -35,32 +35,34 @@ impl<C, H> core::ops::DerefMut for Process<C, H> {
     }
 }
 
-impl<C, H: Default> Process<C, H> {
+impl<C, H> Process<C, H> {
     pub fn new(content: C, code: Vec<CodeLine<C, H>>) -> Self {
         Self {
             code: std::rc::Rc::new(code),
             content,
-            
+
             instruction_ptr: 0,
-            curr_hint: H::default(),
+            curr_hint: None,
         }
     }
 }
 
 impl<C: Clone, H: Clone> Process<C, H> {
     fn execute_fork(&mut self, manager: &mut Manager<C, H>) -> () {
-        for codeline in &self.code.clone()[self.instruction_ptr..] {           
-            
-            let mut hints = codeline(&mut self.content, &self.curr_hint); // Mutates the process and returns the list of hints for branching
+        for codeline in &self.code.clone()[self.instruction_ptr..] {
+            let mut hints = codeline(&mut self.content, self.curr_hint.take()); // Mutates the process and returns the list of hints for branching
             self.instruction_ptr += 1; // Increment the instruction ptr so if the process needs resuming, it knows where to go
-            
-            if hints.len() > 0 { // If any hints were created, they need to be destributed
-                if hints.len() > 1 { // If more than one hint was generated, then forks need to occur
-                    for hint in hints.drain(1..) { // Drain the hints pool up from index 1, leaving 0 for the original
+
+            if hints.len() > 0 {
+                // If any hints were created, they need to be destributed
+                if hints.len() > 1 {
+                    // If more than one hint was generated, then forks need to occur
+                    for hint in hints.drain(1..) {
+                        // Drain the hints pool up from index 1, leaving 0 for the original
                         manager.fork(self, hint); // clone the process and assign the hint (nothing else. The manager is not responsible for the execution of code)
                     }
                 }
-                self.curr_hint = hints.pop().unwrap(); // There will be only one entry in the vector now, time to pop this
+                self.curr_hint = hints.pop(); // There will be only one entry in the vector now, time to pop this
             }
         }
 
@@ -71,19 +73,20 @@ impl<C: Clone, H: Clone> Process<C, H> {
 impl<C, H> Process<C, H> {
     pub fn execute(&mut self) -> () {
         for codeline in self.code.iter() {
-            let mut hints = codeline(&mut self.content, &self.curr_hint); // Mutates the process and returns the list of hints for branching
-            match hints.pop() {
+            let mut hints = codeline(&mut self.content, self.curr_hint.take()); // Mutates the process and returns the list of hints for branching
+            self.curr_hint = hints.pop();
+            /*match hints.pop() {
                 Some(val) => self.curr_hint = val,
                 None => {}
-            }
+            }*/
         }
     }
 }
 
 impl<C, H> Process<C, H> {
-    fn set_hint(&mut self, hint: H) {
-        self.curr_hint = hint
-    }    
+    fn set_hint(&mut self, hint: H) -> () {
+        self.curr_hint = Some(hint)
+    }
 }
 
 pub struct Manager<C, H> {
@@ -91,14 +94,19 @@ pub struct Manager<C, H> {
     clock: usize,
 }
 
+impl<C, H> Manager<C, H> {
+    pub fn iter(&self) -> impl Iterator<Item = &C> {
+        self.vec.iter().map(|process| &process.content)
+    }
+}
 
 impl<C: std::fmt::Debug, H> std::fmt::Debug for Manager<C, H> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Clock: {}, Processes: {:?}", self.clock(), self.vec)
     }
-} 
+}
 
-impl<C, H: Default> Manager<C, H> {
+impl<C, H> Manager<C, H> {
     pub fn new(content: C, code: Vec<CodeLine<C, H>>) -> Self {
         Self {
             vec: vec![Process::new(content, code)],
@@ -132,7 +140,7 @@ impl<C: Clone, H: Clone> Manager<C, H> {
     fn fork(&mut self, process: &mut Process<C, H>, hint: H) {
         let mut new_process = process.clone();
         new_process.set_hint(hint);
-        
+
         self.add_process(new_process);
     }
 }
@@ -141,7 +149,7 @@ impl<C: Clone, H: Clone> Manager<C, H> {
     pub fn execute(&mut self) {
         let mut start_index = 0;
         let mut temp_manager = Manager::empty();
-        
+
         loop {
             for process in &mut self.vec[start_index..] {
                 process.execute_fork(&mut temp_manager);
@@ -150,39 +158,31 @@ impl<C: Clone, H: Clone> Manager<C, H> {
             if temp_manager.vec.len() == 0 {
                 break;
             }
-            
+
             start_index = self.vec.len();
 
             self.vec.extend(temp_manager.vec.drain(..));
         }
-        
+
         self.clock += 1
     }
 }
 
-/* impl<C: HasHint> Manager<C> {    
-    pub fn run(&mut self) {
-        for process in &mut self.vec {
-            process.execute();
-        }
-        
-        self.clock += 1
-    }
-} */
-
-impl<C: Ord, H> Manager<C, H> {   
+impl<C: Ord, H> Manager<C, H> {
     pub fn prune(&mut self, range: usize, step: usize) {
         if self.vec.len() > range {
             self.vec.sort_unstable_by(
-                |a, b| std::cmp::Ord::cmp(&b.content, &a.content) // backwards because want greatest at front
+                |a, b| b.content.cmp(&a.content), // backwards because want greatest at front
             );
 
-            let mut temp = 
-                self.vec
-                .drain(range + step..)
-                .step_by(step)
-                .collect();
+            let mut temp = self.vec.drain(range + step..).step_by(step).collect();
             self.vec.append(&mut temp);
         }
+    }
+}
+
+impl<C: Ord, H> Manager<C, H> {
+    pub fn best(&self) -> Option<&C> {
+        self.iter().max()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
     let code: Vec<fork_model::CodeLine<i32, i32>> = vec![
-        |model: &mut i32, _: &i32| { *model += 1; return vec![1, 2]},
-        |model: &mut i32, hint: &i32| {*model += hint; return vec![]}
+        |model: &mut i32, _: Option<i32>| {
+            *model += 1;
+            return vec![1, 2];
+        },
+        |model: &mut i32, hint: Option<i32>| {
+            *model += hint.unwrap();
+            return vec![];
+        },
     ];
     let mut manager = fork_model::Manager::new(0, code.clone());
 
